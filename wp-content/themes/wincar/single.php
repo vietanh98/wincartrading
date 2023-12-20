8a0fddd1c9201eb59c8c81254ff9799a237d2ad0
anhnv@9pay.vn <anhnv@9pay.vn>
2023-12-20 17:43:39 +0700

    update

--- wp-content/themes/wincar/single.php
+++
@@ -1,9 +0,0 @@
-
-
-<?php while (have_posts()) : the_post(); ?>
-
-	<?php get_template_part('content', 'single'); ?>
-
-	<?php wp_link_pages(array('before' => '<div class="page-links">' . __('Pages:', 'experon'), 'after'  => '</div>',)); ?>
-
-<?php endwhile; ?>
