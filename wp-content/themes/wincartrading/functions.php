272e00026b668b84b6ce368314753f528e8f0c5d
anhnv@9pay.vn <anhnv@9pay.vn>
2023-12-20 18:02:40 +0700

    update code

---
+++ wp-content/themes/wincartrading/functions.php
@@ -0,0 +1,32 @@
+<?php
+// Add theme support for featured images
+add_theme_support( 'post-thumbnails' );
+
+// Register a primary menu location
+register_nav_menus( array(
+  'primary' => __( 'Primary Menu', 'my-theme' ),
+) );
+
+// Enqueue theme styles and scripts
+function my_theme_enqueue_scripts() {
+  // Enqueue style.css
+  wp_enqueue_style( 'my-theme-style', get_stylesheet_uri() );
+  // Enqueue custom.js
+  wp_enqueue_script( 'my-theme-script', get_template_directory_uri() . '/js/custom.js', array( 'jquery' ), '1.0.0', true );
+}
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_scripts' );
+
+// Register a sidebar widget area
+function my_theme_widgets_init() {
+  register_sidebar( array(
+    'name'          => __( 'Sidebar', 'my-theme' ),
+    'id'            => 'sidebar-1',
+    'description'   => __( 'Add widgets here to appear in your sidebar.', 'my-theme' ),
+    'before_widget' => '<div id="%1$s" class="widget %2$s">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<h3 class="widget-title">',
+    'after_title'   => '</h3>',
+  ) );
+}
+add_action( 'widgets_init', 'my_theme_widgets_init' );
+?>
